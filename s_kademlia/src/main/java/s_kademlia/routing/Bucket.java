4be3a3c876cf0a420645cbf8e531e87f7ddd6ab2
luismdsleite <luismdsleite@gmail.com>
2023-05-07 15:01:38 +0100

    Create insertNode() method to the Bucket class

--- s_kademlia/src/main/java/s_kademlia/routing/Bucket.java
+++ s_kademlia/src/main/java/s_kademlia/routing/Bucket.java
@@ -1,11 +1,12 @@
 package s_kademlia.routing;
 
+import java.util.NoSuchElementException;
 import java.util.TreeSet;
 
 import s_kademlia.node.Node;
 
 /**
- * Represents an S/Kademlia bucket 
+ * Represents an S/Kademlia bucket
  */
 public class Bucket {
     private TreeSet<Contact> contacts;
@@ -15,17 +16,21 @@ public class Bucket {
         this.contacts = new TreeSet<>();
         this.K_Nodes = k;
     }
-    
+
     /**
-     * <p> Adds a Node to the bucket.
-     * <p> If the node was already in the bucket then we update its timestamp.
+     * <p>
+     * Adds a Node to the bucket.
+     * <p>
+     * If the node was already in the bucket then we update its timestamp.
+     * 
      * @param contact
      * @return
      */
     public boolean addNode(Node node) {
         Contact contact = new Contact(node);
-        // If we already had a contact then remove it, update its timestamp and add it again.
-        if (contacts.contains(contact)){
+        // If we already had a contact then remove it, update its timestamp and add it
+        // again.
+        if (contacts.contains(contact)) {
             contacts.remove(contact);
             contact.updateLastSeen();
             contacts.add(contact);
@@ -37,13 +42,40 @@ public class Bucket {
             return true;
         }
         // If the bucket is full, remove the oldest contact and insert the new one.
-        else{
+        else {
             Contact OldestSeen = contacts.pollFirst();
             contacts.remove(OldestSeen);
             contacts.add(contact);
             return true;
         }
-        
+
+    }
+
+    public synchronized void insert(Contact c){
+        if(this.contacts.contains(c)){
+            /**
+             * If the contact is already in the bucket, then update it and put it at the head of the list.
+             * TODO:
+             */
+            contacts.remove(c);
+        }
+    }
+
+    /**
+     * Removes a node from the bucket.
+     * 
+     * @param n Node to remove
+     * @return True if the contact was removed, false if the node is not present in
+     *         the bucket.
+     */
+    public synchronized Contact remove(Node n) {
+        for (Contact c : contacts) {
+            if (c.getNode().equals(n)) {
+                contacts.remove(c);
+                return c;
+            }
+        }
+        throw new NoSuchElementException("Node is not present in the bucket.");
     }
 
     public boolean removeNode(Node node) {
